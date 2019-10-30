9fa062448c8f1ed9a018cc6ecda70265b0d621c6
Mominis <46275902+kkrnme@users.noreply.github.com>
2019-10-31 03:32:44 +0900

    mesasge not found

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default () => (
+  <h1
+    css={{
+      fontSize: `750%`,
+      textAlign: `center`,
+      fontFamily: `serif`,
+      margin: 0,
+    }}
+  >
+    <a href="/">404 Not Found</a>
+  </h1>
+)
