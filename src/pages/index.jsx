42e3bca48b58c776e46c2ad97ad38864416e3058
Mominis <46275902+kkrnme@users.noreply.github.com>
2019-10-31 03:33:28 +0900

    UNCO解除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,15 +2,22 @@ import React from "react"
 import { graphql } from "gatsby"
 import { SiteHeader, SiteFooter } from "../components/HeaderFooter"
 import { TopStyle } from "../components/style"
+import Logo from "../components/kkrnme_logo.svg"
 
 export default ({ data }) => (
   <div>
     <TopStyle />
     <SiteHeader />
     <main>
-      <h1>UNDER CONSTRUCTION</h1>
-      <p>kkrn.me</p>
+      <h1>KKRN.ME</h1>
       <p>{data.site.siteMetadata.discription}</p>
+      <img
+        src={Logo}
+        alt="KKRN.ME"
+        css={{
+          width: `80%`,
+        }}
+      />
     </main>
     <SiteFooter />
   </div>
