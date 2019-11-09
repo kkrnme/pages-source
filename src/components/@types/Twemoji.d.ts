17abdfece9750204dae16cd67065279b7a43e78d
Mominis <46275902+kkrnme@users.noreply.github.com>
2019-11-09 16:26:11 +0900

    Type definetion(?) for react-twemoji

--- src/components/@types/Twemoji.d.ts
+++ src/components/@types/Twemoji.d.ts
@@ -1,1 +1,25 @@
-declare module "react-twemoji"
+declare module "react-twemoji" {
+  import React from "react"
+  import { ParseObject } from "twemoji"
+
+  class ReactTwemoji extends React.Component<ReactTwemoji.TwemojiProps> {}
+
+  namespace ReactTwemoji {
+    type TwemojiProps =
+      | {
+          noWrapper: true
+          options?: TwemojiOptions
+          children: React.ReactNode
+        }
+      | {
+          noWrapper?: false
+          options?: TwemojiOptions
+          tag?: string
+          children: React.ReactNode
+        }
+    type TwemojiOptions = ParseObject
+  }
+  export type TwemojiOptions = ReactTwemoji.TwemojiOptions
+  export type TwemojiProps = ReactTwemoji.TwemojiProps
+  export default ReactTwemoji
+}
