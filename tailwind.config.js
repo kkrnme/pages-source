631a98ade20bf5bd684413f70f00f59af6b5c123
Mominis <46275902+kkrnme@users.noreply.github.com>
2019-11-11 23:41:24 +0900

    postcss/tailwind test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      xs: "1rem",
+      sm: "1.125rem",
+      base: "1.25rem",
+      lg: "1.5rem",
+      xl: "1.875rem",
+      "2xl": "2.25rem",
+      "3xl": "3rem",
+      "4xl": "4rem",
+    },
+    extend: {
+      colors: {
+        gatsbyPurple: {
+          "50": `#8a4baf`,
+          "60": "#663399",
+          "80": `#452475`,
+          "90": `#362066`,
+        },
+        gatsbyBlack: `#131217`,
+        react: `#61dafb`,
+        reactBlack: `#20232a`,
+        reactShadow: `#282c34`,
+        graphqlBlack: `#171e26`,
+        graphqlDarkWash: `#3d464f`,
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
