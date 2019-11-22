fc3d5ef74585b4284abdeb731ee0a7beba2dbf39
Mominis <46275902+kkrnme@users.noreply.github.com>
2019-11-23 00:37:16 +0900

    emojiをtailwindに移す

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,9 @@ import React from "react"
 import CenterdWrapper from "../components/wrappers/CenterdWrapper"
 
 export default () => (
-  <div>
-    <CenterdWrapper>
-      <h1>KKRN.ME</h1>
-      <p>page of mominis</p>
-      <h2></h2>
-    </CenterdWrapper>
-  </div>
+  <CenterdWrapper>
+    <h1>KKRN.ME</h1>
+    <p>page of mominis</p>
+    <h2></h2>
+  </CenterdWrapper>
 )
