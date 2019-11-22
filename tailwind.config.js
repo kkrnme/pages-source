de2c6afb63f0f1a3bb9072dee5f5b1cccca85773
Mominis <46275902+kkrnme@users.noreply.github.com>
2019-11-23 01:34:53 +0900

    LinkTopostもtailwind化

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       "4xl": "4rem",
     },
     letterSpacing: {
-      "09": "0.09em"
+      "09": "0.09em",
     },
     extend: {
       colors: {
@@ -35,17 +35,18 @@ module.exports = {
   },
   variants: {},
   plugins: [
-    ({addUtilities}) => addUtilities({
-      ".emoji":{
-        display: `inline`,
-        verticalAlign: `baseline`,
-        height: `1.2em`,
-        transform: `translateY(0.2em)`,
-        transition: `300ms all cubic-bezier(.68,-0.55,.27,1.55)`,
-        "&:hover": {
-          transform: `translateY(0.2em) scale(2)`,
+    ({ addUtilities }) =>
+      addUtilities({
+        ".emoji": {
+          display: `inline`,
+          verticalAlign: `baseline`,
+          height: `1.2em`,
+          transform: `translateY(0.2em)`,
+          transition: `300ms all cubic-bezier(.68,-0.55,.27,1.55)`,
+          "&:hover": {
+            transform: `translateY(0.2em) scale(2)`,
+          },
         },
-      },
-    })
+      }),
   ],
 }
