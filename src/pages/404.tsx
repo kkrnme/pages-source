005529ed36281458349feee1d84a7b2d34777741
Mominis <46275902+kkrnme@users.noreply.github.com>
2019-12-27 00:22:42 +0900

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <div className="bg-gray-900 w-full h-screen absolute text-yellow-300 text-center text-base font-serif">
     <h1 className="text-400  ">404 Not Found</h1>
     <p>ページが見当たらなかった。</p>
     <p>
-      <a href="/">トップに戻る</a>
+      <Link to="/">トップに戻る</Link>
     </p>
   </div>
 )
